a442c929dcd482b514f342d0d868c01eb40f72fc
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-06-24 10:20:14 -0700

    added main.h

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct printer - Struct printer
+ * @format: format to print
+ * @f: function to print
+ */
+
+typedef struct printer
+{
+	char *format;
+	int (*f)(va_list);
+} printer;
+
+int _putchar(char c);
+int print_str(va_list ptr);
+int print_selector(const char *format, va_list ptr);
+int _printf(const char *format, ...);
+
+#endif
