c9a35fa7b698a57554660764183bf9f35f5e91ac
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-06-24 19:09:48 +0000

    updated print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -1,42 +1,58 @@
+#include "main.h"
 /**
  * _putchar - custom putchar function
- * @va_list: list where char to print is located
+ * @C: char to printed
  *
  * Return: 1 (success), -1 (failure)
  */
- int _putchar(va_list)
+ int _putcha(char c)
 {
-	char c = va_arg(list, int);
 	int retval;
 
-	write(1, &c, 1);
+	retval = write(1, &c, 1);
+
+	return (retval);
 }
+
+/**
+ * char_printer - prints chars
+ * @args: list where char is located
+ *
+ * Return: 1 (successful print)
+ */
+int char_printer(va_list args)
+{
+	char c = va_arg(list, int);
+
+	return(_putchar(c));
+}
+
 
 
 
 /**
  * string_printer - prints strings
- * @va_list:list where string to be printed is located
+ * @args: list where string to be printed is located
  *
  * Return: number of chars printed in string
  */
-int string_printer(va_list)
+int string_printer(va_list args)
 {
 	char *s = va_arg(args, char *);
-	int i, print_counter = 0;
+	int i;
 	
 	if (s != NULL)
 	{
 		for (i = 0; s[i] != '\0'; i++)
 		{
 			_putchar(c);
-			print_counter++;
 		}
-		return (print_counter++);
+		return (i);
 	}
 	else
 	{	
-		return 
+		return (-1);
+	}
 
 
 }
