ba48355ac0a78363c071650cd51fcf24a7a70405
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-06-25 14:31:40 -0700

    printf function

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: format to print
+ * @...: arg list
+ * Return: printed character count
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i;
+/*	int (*printer)(va_list);*/
+	int char_count = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				char_count++;
+				_putchar('%');
+			}
+			else if (format[i + 1] != '\0')
+			{
+/*				printer = print_selector(format[i + 1]); */
+				char_count += string_printer(args);
+			}
+			else
+				break;
+		}
+		else
+		{
+			char_count++;
+			_putchar(format[i]);
+		}
+	}
+	return (char_count);
+}
